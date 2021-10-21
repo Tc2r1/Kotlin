a2b3f42b2aefbe9b8a4ccdd497314bcb66db8d56
tc2r1 <nudenniew@gmail.com>
2021-10-21 18:31:10 -0500

    Code Cleanup:
    Kotlin App Upgrades - Code Cleanup:
    Added:
    Kotlin Androiid Extensions View Binding.
    Moved strings to string.xml.
    Reformat Code.

--- Udacity/DiceRoller/app/src/main/java/com/tc2r1/diceroller/MainActivity.kt
+++ Udacity/DiceRoller/app/src/main/java/com/tc2r1/diceroller/MainActivity.kt
@@ -1,32 +1,25 @@
 package com.tc2r1.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var rollButtton: Button
-    lateinit var diceImage: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        rollButtton = findViewById(R.id.btn_roll)
-        diceImage   = findViewById(R.id.iv_dice)
 
-        rollButtton.setOnClickListener {
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+        btn_roll.setOnClickListener {
+            Toast.makeText(this, "Rolling Dice", Toast.LENGTH_SHORT).show()
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -34,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
+        iv_dice.setImageResource(drawableResource)
     }
 
 }
--- Udacity/DiceRoller/app/src/test/java/com/tc2r1/diceroller/ExampleUnitTest.kt
+++ Udacity/DiceRoller/app/src/test/java/com/tc2r1/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.tc2r1.diceroller
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
