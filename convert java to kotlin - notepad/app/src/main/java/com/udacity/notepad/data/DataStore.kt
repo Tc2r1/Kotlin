ebce581d1137963b9501747f0f744493dc175c2e
tc2r1 <nudenniew@gmail.com>
2021-10-21 01:44:02 -0500

    Converted DataStore to Kotlin using Anko to help with Async task.

---
+++ convert java to kotlin - notepad/app/src/main/java/com/udacity/notepad/data/DataStore.kt
@@ -0,0 +1,29 @@
+package com.udacity.notepad.data
+
+import android.content.Context
+import org.jetbrains.anko.doAsync
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+
+object DataStore {
+    val EXEC: Executor = Executors.newSingleThreadExecutor()
+
+    @JvmStatic
+    lateinit var notes: NoteDatabase
+        private set
+
+    @JvmStatic
+    fun init(context: Context) {
+        notes = NoteDatabase(context)
+    }
+
+    @JvmStatic
+    fun execute(runnable: Runnable) {
+        execute { runnable.run() }
+    }
+    @JvmStatic
+    fun execute( fn: () -> Unit) {
+        doAsync { fn() }
+    }
+
+}
