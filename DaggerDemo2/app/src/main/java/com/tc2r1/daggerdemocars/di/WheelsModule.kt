993880da8221570b5934172529ff0ed46dadf9b2
tc2r1 <nudenniew@gmail.com>
2021-10-20 20:03:57 -0500

    Changed wheel to mock of a third party library that depends on Rims and Tires.
    Used Dagger to create instances of Wheel using @module and @Provides

---
+++ DaggerDemo2/app/src/main/java/com/tc2r1/daggerdemocars/di/WheelsModule.kt
@@ -0,0 +1,4 @@
+package com.tc2r1.daggerdemocars.di
+
+class Wheels {
+}
--- DaggerDemo2/app/src/main/java/com/tc2r1/daggerdemocars/objects/mobility/Wheels.kt
+++ DaggerDemo2/app/src/main/java/com/tc2r1/daggerdemocars/objects/mobility/Wheels.kt
@@ -2,6 +2,4 @@ package com.tc2r1.daggerdemocars.objects
 
 import javax.inject.Inject
 
-class Wheels @Inject constructor() {
-
-}
+class Wheels(var rims: Rims, var tires: Tires)
