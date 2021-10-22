3d7157daa522c0bf89c6e508c8f5b464321a6d05
tc2r1 <nudenniew@gmail.com>
2021-10-22 02:15:16 -0500

    Added Code to Activity to set OnClickListener of the Done Button.

--- Udacity/AboutMe/app/src/main/java/com/tc2r1/aboutme/MainActivity.kt
+++ Udacity/AboutMe/app/src/main/java/com/tc2r1/aboutme/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.tc2r1.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_done).setOnClickListener {
+            addNickName(it)
+        }
+
     }
+
+    private fun addNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.edit_nickname)
+        val nickNameTextView = findViewById<TextView>(R.id.tv_nickname)
+
+        nickNameTextView.text = editText.text
+
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+
+        nickNameTextView.visibility = View.VISIBLE
+
+        // Hide Keyboard when complete.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+
 }
