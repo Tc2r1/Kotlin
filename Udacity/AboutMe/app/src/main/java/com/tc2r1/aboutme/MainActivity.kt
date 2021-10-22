5a5b4c2bd71eda4012cfb52bc915de12bb6ae538
tc2r1 <nudenniew@gmail.com>
2021-10-22 02:47:13 -0500

    Added Data Binding to xml and main activity:
    Data Binding creates an object that connects/maps/binds two pieces of distant information together at compile time, so that you dont have to look for it at runtime.
    The object that surfaces these bindings to you is called the Binding Object.
    
        Updating data and then updating the data displayed in views is cumbersome and a source of errors. Keeping the data in the view also violates separation of data and presentation.
        Data binding solves both of these problems. You keep data in a data class. You add a <data> block to the <layout> to identify the data as variables to use with the views. Views reference the variables.
        The compiler generates a binding object that binds the views and data.
        In your code, you reference and update the data through the binding object, which updates the data, and thus what is displayed in the view.
        Binding views to data sets a foundation for more advanced techniques using data binding.

--- Udacity/AboutMe/app/src/main/java/com/tc2r1/aboutme/MainActivity.kt
+++ Udacity/AboutMe/app/src/main/java/com/tc2r1/aboutme/MainActivity.kt
@@ -1,37 +1,49 @@
 package com.tc2r1.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.tc2r1.aboutme.data.MyName
+import com.tc2r1.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Nudennie White")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_done).setOnClickListener {
-            addNickName(it)
+        // Without Data Binding
+        //setContentView(R.layout.activity_main)
+        //        findViewById<Button>(R.id.btn_done).setOnClickListener {
+        //            addNickName(it)
+        //        }
+
+
+        // With Data Binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.apply {
+
+            btnDone.setOnClickListener {
+                addNickName(it)
+            }
         }
-
     }
 
     private fun addNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.edit_nickname)
-        val nickNameTextView = findViewById<TextView>(R.id.tv_nickname)
 
-        nickNameTextView.text = editText.text
-
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-
-        nickNameTextView.visibility = View.VISIBLE
-
+        binding.apply {
+            myName?.nickName = etNickname.text.toString()
+            invalidateAll()
+            etNickname.visibility = View.GONE
+            btnDone.visibility = View.GONE
+            tvNickname.visibility = View.VISIBLE
+        }
         // Hide Keyboard when complete.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
---
+++ Udacity/AboutMe/app/src/main/java/com/tc2r1/aboutme/data/MyName.kt
@@ -0,0 +1,3 @@
+package com.tc2r1.aboutme.data
+
+data class MyName(var name: String = "", var nickName: String = "")
