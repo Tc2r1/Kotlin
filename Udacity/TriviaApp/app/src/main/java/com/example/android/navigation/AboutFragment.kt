69fe46b74456e6d01fbe37db427adaea6a807501
tc2r1 <nudenniew@gmail.com>
2021-10-24 06:54:26 -0500

    Code Cleanup

--- Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -25,7 +25,6 @@ import android.view.ViewGroup
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
--- Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -39,6 +41,13 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.myNavHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        navController.addOnDestinationChangedListener{ controller, destination, bundle  ->
+            when(destination.id) {
+                controller.graph.startDestination -> drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+                else -> drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+
+        }
         NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
