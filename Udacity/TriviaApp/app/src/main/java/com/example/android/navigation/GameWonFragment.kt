69f2fecd8b2a31a4b3cea27e890f27f921407a68
tc2r1 <nudenniew@gmail.com>
2021-10-24 06:10:17 -0500

    Added share button to action menu with intents to share score.

--- Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,11 +16,10 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.view.*
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -42,15 +41,46 @@ class GameWonFragment : Fragment() {
                 view.findNavController()
                     .navigate(GameWonFragmentDirections.actionGameWonFragmentToTitleFragment())
             }
-
-            val args: GameWonFragmentArgs by navArgs()
-
-            Toast.makeText(
-                context,
-                "NumCorrect: ${args.numCorrect} out of ${args.numQuestions}",
-                Toast.LENGTH_LONG
-            ).show()
         }
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+
+        // check if the activity resolves
+        getShareIntent().resolveActivity(requireActivity().packageManager).let {
+            // hide the share menu item.
+            menu.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent(): Intent {
+        val args: GameWonFragmentArgs by navArgs()
+
+        // Alternative way of creating intent for sharing.
+//        val shareIntent = Intent(Intent.ACTION_SEND)
+//        shareIntent.type = "text/plain"
+//        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+//        return shareIntent
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .setText("text/plain")
+            .intent
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+
     }
 }
