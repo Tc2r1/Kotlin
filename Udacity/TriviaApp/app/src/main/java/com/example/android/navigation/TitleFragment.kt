b096a7b70b02df356b8890172e589cd03cebdb6e
tc2r1 <nudenniew@gmail.com>
2021-10-24 03:46:58 -0500

    Added Navigation Graph Resource Added NavHostFragment Added Fragments to the Navigation Graph Connected fragments in the graph with actions Created onClickListener Linked Navigation Controller to navigate with Action

--- Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ Udacity/TriviaApp/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -21,6 +23,9 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title, container, false)
+        binding.apply {
+            playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        }
         return binding.root
     }
 }
