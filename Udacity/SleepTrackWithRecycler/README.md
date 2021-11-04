ad13fc3b02230aca8030dc12e31968cfe05b6d12
tc2r1 <nudenniew@gmail.com>
2021-11-03 23:44:49 -0500

    Init for Lesson 7 of Udacity's course: https://classroom.udacity.com/courses/ud9012

---
+++ Udacity/SleepTrackWithRecycler/README.md
@@ -0,0 +1,34 @@
+# RecyclerView - SleepQualityTracker with RecyclerView app
+
+This is the toy app for Lesson 7 of the [Android App Development in Kotlin course on Udacity](https://classroom.udacity.com/courses/ud9012/).
+
+
+The SleepQualityTracker app is a demo app that helps you collect information about your sleep.
+* Start time
+* End time
+* Quality
+* Time slept
+
+This app demonstrates the following views and techniques:
+* Room database
+* DAO
+* Coroutines
+
+It also uses and builds on the following techniques from previous lessons:
+* Transformation map
+* Data Binding in XML files
+* ViewModel Factory
+* Using Backing Properties to protect MutableLiveData
+* Observable state LiveData variables to trigger navigation
+## SleepQualityTracker with RecyclerView
+
+This app builds on the SleepQualityTracker you developed in Lesson 6.  In this lesson you'll learn how to display a list of results in a RecyclerView instead of a static ScrollView. You'll also learn how you can refactor your code to make it more efficient so it will be easier to maintain and test.
+
+## Screenshots
+
+![Screenshot1](screenshots/sleep_tracker_recycler_home.png)
+![Screenshot2](screenshots/sleep_tracker_recycler_detail.png)
+
+
+
+
---
+++ Udacity/SleepTrackWithRecycler/app/src/main/java/com/nudennie/trackmysleepquality/sleepdetail/SleepDetailViewModel.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nudennie.trackmysleepquality.sleepdetail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.nudennie.trackmysleepquality.database.SleepDatabaseDao
+import com.nudennie.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.Job
+
+/**
+ * ViewModel for SleepQualityFragment.
+ *
+ * @param sleepNightKey The key of the current night we are working on.
+ */
+class SleepDetailViewModel(
+        private val sleepNightKey: Long = 0L,
+        dataSource: SleepDatabaseDao) : ViewModel() {
+
+    /**
+     * Hold a reference to SleepDatabase via its SleepDatabaseDao.
+     */
+    val database = dataSource
+
+
+    /**
+     */
+
+    private val night = MediatorLiveData<SleepNight>()
+
+    fun getNight() = night
+
+    init {
+        night.addSource(database.getNightWithId(sleepNightKey), night::setValue)
+    }
+
+    /**
+     * Variable that tells the fragment whether it should navigate to [SleepTrackerFragment].
+     *
+     * This is `private` because we don't want to expose the ability to set [MutableLiveData] to
+     * the [Fragment]
+     */
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+
+    /**
+     * When true immediately navigate back to the [SleepTrackerFragment]
+     */
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    /**
+     *
+     */
+
+
+    /**
+     * Call this immediately after navigating to [SleepTrackerFragment]
+     */
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onClose() {
+        _navigateToSleepTracker.value = true
+    }
+
+}
