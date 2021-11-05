e66ec67daeaa19ee5f5860434dcb644cbb08ff85
tc2r1 <nudenniew@gmail.com>
2021-11-05 01:18:31 -0500

    implementing on click listeners. Passed click listener from the view all the way to the recyclerview viewholder.

--- Udacity/GridLayout/app/src/main/java/com/nudennie/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Udacity/GridLayout/app/src/main/java/com/nudennie/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -19,33 +19,32 @@ package com.nudennie.trackmysleepquality.sleeptracker
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.*
-import com.nudennie.trackmysleepquality.*
 import com.nudennie.trackmysleepquality.database.SleepNight
 import com.nudennie.trackmysleepquality.databinding.ListItemSleepNightBinding
 import com.nudennie.trackmysleepquality.sleeptracker.SleepNightAdapter.ViewHolder
 
-class SleepNightAdapter : ListAdapter<SleepNight, ViewHolder>(SleepNightDiffCallback()) {
+class SleepNightAdapter(val clickListener: SleepNightListener) : ListAdapter<SleepNight, ViewHolder>(SleepNightDiffCallback()) {
 
-    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent = parent)
     }
 
-    override fun onBindViewHolder(holder : ViewHolder, position : Int) {
-        val sleepNight = getItem(position)
-        holder.bind(sleepNight)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position)!!, clickListener)
     }
 
-    class ViewHolder private constructor(val binding : ListItemSleepNightBinding) :
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(sleepNight : SleepNight) {
+        fun bind(sleepNight: SleepNight, clickListener: SleepNightListener) {
+            binding.clickListener = clickListener
             binding.sleep = sleepNight
             binding.executePendingBindings()
 
         }
 
         companion object {
-            fun from(parent : ViewGroup) : ViewHolder {
+            fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
                 return ViewHolder(binding)
@@ -55,12 +54,16 @@ class SleepNightAdapter : ListAdapter<SleepNight, ViewHolder>(SleepNightDiffCall
 }
 
 class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
-    override fun areItemsTheSame(oldItem : SleepNight, newItem : SleepNight) : Boolean {
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         return oldItem.nightId == newItem.nightId
     }
 
-    override fun areContentsTheSame(oldItem : SleepNight, newItem : SleepNight) : Boolean {
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         return oldItem == newItem
     }
 
+}
+
+class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
+    fun onClick(night: SleepNight) = clickListener(night.nightId)
 }
