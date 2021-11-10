25c5be9bf517456b7d957182bfe63801a1b08132
tc2r1 <nudenniew@gmail.com>
2021-11-09 23:31:30 -0600

    added DAO

--- Udacity/OfflineCaching/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ Udacity/OfflineCaching/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -16,3 +16,38 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface VideoDao {
+
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg video: DatabaseVideo)
+
+}
+
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideoDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideoDatabase
+
+
+fun getDatabase(context: Context): VideoDatabase {
+    synchronized(VideoDatabase::class.java) {
+        if(!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext, VideoDatabase::class.java, "videos")
+                .build()
+        }
+    }
+    return INSTANCE
+}
