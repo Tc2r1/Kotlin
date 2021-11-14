7b7e93dd3059e2037c71b7a99f09cb7977165523
Tc2r1 <Tc2r1@yahoo.com>
2021-11-13 23:35:42 -0600

    init commit

---
+++ Udacity/DesignForEveryone/app/src/main/java/com/nudennie/gdgfinder/add/AddGdgFragment.kt
@@ -0,0 +1,45 @@
+package com.nudennie.gdgfinder.add
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+
+import com.nudennie.gdgfinder.R
+import com.nudennie.gdgfinder.databinding.AddGdgFragmentBinding
+import com.google.android.material.snackbar.Snackbar
+
+class AddGdgFragment : Fragment() {
+
+    private val viewModel: AddGdgViewModel by lazy {
+        ViewModelProvider(this).get(AddGdgViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = AddGdgFragmentBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.setLifecycleOwner(this)
+
+        binding.viewModel = viewModel
+
+        viewModel.showSnackBarEvent.observe(this, Observer {
+            if (it == true) { // Observed state is true.
+                Snackbar.make(
+                    activity!!.findViewById(android.R.id.content),
+                    getString(R.string.application_submitted),
+                    Snackbar.LENGTH_SHORT // How long to display the message.
+                ).show()
+                viewModel.doneShowingSnackbar()
+            }
+        })
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+}
---
+++ Udacity/DesignForEveryone/app/src/main/java/com/nudennie/gdgfinder/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.nudennie.gdgfinder.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.nudennie.gdgfinder.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        return view
+    }
+}
