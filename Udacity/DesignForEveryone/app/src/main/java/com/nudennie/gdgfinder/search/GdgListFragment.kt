4f88ca4f78b2c01b45109cd7a0748d4943f73231
Tc2r1 <Tc2r1@yahoo.com>
2021-12-01 23:41:38 -0600

    Added ChipGroup to project.

--- Udacity/DesignForEveryone/app/src/main/java/com/nudennie/gdgfinder/search/GdgListFragment.kt
+++ Udacity/DesignForEveryone/app/src/main/java/com/nudennie/gdgfinder/search/GdgListFragment.kt
@@ -11,9 +11,11 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.nudennie.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
+import com.nudennie.gdgfinder.R
+import com.nudennie.gdgfinder.databinding.FragmentGdgListBinding
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -21,41 +23,79 @@ private const val LOCATION_PERMISSION = "android.permission.ACCESS_FINE_LOCATION
 
 class GdgListFragment : Fragment() {
 
-
     private val viewModel: GdgListViewModel by lazy {
         ViewModelProvider(this).get(GdgListViewModel::class.java)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val binding = FragmentGdgListBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
-        val adapter = GdgListAdapter(GdgClickListener { chapter ->
-            val destination = Uri.parse(chapter.website)
-            startActivity(Intent(Intent.ACTION_VIEW, destination))
-        })
+        val adapter = GdgListAdapter(
+            GdgClickListener { chapter ->
+                val destination = Uri.parse(chapter.website)
+                startActivity(Intent(Intent.ACTION_VIEW, destination))
+            }
+        )
 
         // Sets the adapter of the RecyclerView
         binding.gdgChapterList.adapter = adapter
 
-        viewModel.showNeedLocation.observe(viewLifecycleOwner, object: Observer<Boolean> {
-            override fun onChanged(show: Boolean?) {
-                // Snackbar is like Toast but it lets us show forever
-                if (show == true) {
-                    Snackbar.make(
-                        binding.root,
-                        "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
-                        Snackbar.LENGTH_LONG
-                    ).show()
+        viewModel.showNeedLocation.observe(
+            viewLifecycleOwner,
+            object : Observer<Boolean> {
+                override fun onChanged(show: Boolean?) {
+                    // Snackbar is like Toast but it lets us show forever
+                    if (show == true) {
+                        Snackbar.make(
+                            binding.root,
+                            "No location. Enable location in settings (hint: test with Maps) then check app permissions!",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
                 }
             }
-        })
+        )
+
+        viewModel.regionList.observe(
+            viewLifecycleOwner,
+            object : Observer<List<String>> {
+                override fun onChanged(data: List<String>?) {
+                    data ?: return
+
+                    // 1: Make a new Chip view for each item in the list
+                    val chipGroup = binding.regionsList
+                    val inflator = LayoutInflater.from(chipGroup.context)
+
+                    val children = data.map { regionName ->
+                        val chip = inflator.inflate(R.layout.regions, chipGroup, false) as Chip
+                        chip.text = regionName
+                        chip.tag = regionName
+                        chip.setOnCheckedChangeListener { button, isChecked ->
+                            viewModel.onFilterChanged(button.tag as String, isChecked)
+                        }
+                         chip
+                    }
+
+                    // 2: Remove any views already in the ChipGroup
+                    chipGroup.removeAllViews()
+
+                    // 3: Add the new children to the ChipGroup.
+                    children.forEach {
+                        chipGroup.addView(it)
+                    }
+                }
+            }
+        )
 
         setHasOptionsMenu(true)
         return binding.root
@@ -81,7 +121,11 @@ class GdgListFragment : Fragment() {
      */
     private fun requestLastLocationOrStartLocationUpdates() {
         // if we don't have permission ask for it and wait until the user grants it
-        if (ContextCompat.checkSelfPermission(requireContext(), LOCATION_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                LOCATION_PERMISSION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             requestLocationPermission()
             return
         }
@@ -102,14 +146,17 @@ class GdgListFragment : Fragment() {
      */
     private fun startLocationUpdates(fusedLocationClient: FusedLocationProviderClient) {
         // if we don't have permission ask for it and wait until the user grants it
-        if (ContextCompat.checkSelfPermission(requireContext(), LOCATION_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                LOCATION_PERMISSION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             requestLocationPermission()
             return
         }
 
-
         val request = LocationRequest().setPriority(LocationRequest.PRIORITY_LOW_POWER)
-        val callback = object: LocationCallback() {
+        val callback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult?) {
                 val location = locationResult?.lastLocation ?: return
                 viewModel.onLocationUpdated(location)
@@ -123,9 +170,13 @@ class GdgListFragment : Fragment() {
      *
      * If granted, continue with the operation that the user gave us permission to do.
      */
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode) {
+        when (requestCode) {
             LOCATION_PERMISSION_REQUEST -> {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     requestLastLocationOrStartLocationUpdates()
@@ -134,5 +185,3 @@ class GdgListFragment : Fragment() {
         }
     }
 }
-
-
