5c7b9078e54488bd718c097a49b0dca91bbd5ecc
Tc2r1 <Tc2r1@yahoo.com>
2022-04-24 22:06:16 -0500

    Created  The Layout In Jetpack Compose

--- Android_Coldelabs/ComposeTutorial/app/src/main/java/com/nudennie/composetutorial/MainActivity.kt
+++ Android_Coldelabs/ComposeTutorial/app/src/main/java/com/nudennie/composetutorial/MainActivity.kt
@@ -3,11 +3,22 @@ package com.nudennie.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nudennie.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,33 +28,43 @@ class MainActivity : ComponentActivity() {
             ComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard(Message("Nudennie", "Jetpack Compose"))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact Profile Picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        // Add a Horizontal Spacer
+        Spacer(modifier = Modifier.size(4.dp))
+
+        Column{
+            Text(text = msg.author)
+            // Adds a vertical spacer?
+            Spacer(modifier = Modifier.size(6.dp))
+
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Nudennie")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeTutorialTheme {
-        Greeting("Android")
-    }
+    MessageCard(
+        msg = Message("Nudennie", "Jetpack Compose")
+    )
 }
